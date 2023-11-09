a62619873724ba549070b171623e4da3d7ff1e7c
Your Name <you@example.com>
2023-11-09 14:24:35 +0900

    라우팅 및 컴퍼넌트 셋팅

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Header from '../components/Header'
+import Skip from '../components/Skip'
+import Skill from '../components/Skill'
+import Intro from '../components/Intro'
+import Site from '../components/Site'
+import Port from '../components/Port'
+import Footer from '../components/Footer'
+import Contact from '../components/Contact'
+import Main from '../components/Main'
+
+
+
+const HomeView = () => {
+  return (
+      <>
+            <Skip />
+            <Header />
+            <Main>
+              <Intro />
+              <Skill />
+              <Site />
+              <Port />
+              <Contact />
+            </Main>
+            <Footer />
+            
+      </>
+  )
+}
+
+
+
+export default HomeView
